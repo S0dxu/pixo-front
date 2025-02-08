691f958ff4eeef51afaeb3b6f3741780a4e9f7f3
S0dxu <159276909+S0dxu@users.noreply.github.com>
2025-02-08 23:34:32 +0100

    Update Create.jsx

--- src/pages/Create/Create.jsx
+++ src/pages/Create/Create.jsx
@@ -24,17 +24,11 @@ const Create = () => {
     const validateFile = (file) => {
         if (file && allowedTypes.includes(file.type)) {
             setFile(file);
-            uploadImage(file, clientId).then(response => {
-                if (response && response.data && response.data.link) {
-                    const link = response.data.link;
-                    console.log(link);
-                    navigate("./../upload", { state: { file, link } });
-                } else {
-                    setError("Error while image")
-                }
+            uploadImage(file, clientId).then(link => {
+                console.log(link);
+                navigate("./../upload", { state: { file, link } });
             }).catch(error => {
                 console.error("error uploading image:", error);
-                setError("Error while image")
             });
             setError("");
         } else {
