ca1b726efd5cf581620587d9a2ee600e032045ea
S0dxu <159276909+S0dxu@users.noreply.github.com>
2025-02-17 16:55:40 +0100

    like color
    red = liked else black

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -170,9 +170,18 @@ const Player = () => {
             setViews(data.views);
             setTextToCopy(`https://pixo-v1.netlify.app/${data._id}`);
             setCurrentImageId(data._id);
-            setLikes(data.likes);
+
+            const userId = getUserIdFromToken();
+            const likes = data.likes;
+            if (likes.some(like => like.toString() === userId)) {
+                setIsLiked(true);
+            } else {
+                setIsLiked(false);
+            }
+
+            setLikes(likes.length)
+
             console.log("ID:", data._id);
-            console.log(songlink)
         } catch (error) {
             console.error("another error :", error); // type shit
         } finally {
@@ -187,7 +196,7 @@ const Player = () => {
         try {
             const response = await fetch("https://pixo-backend-version-1-2.onrender.com/get-random-image");
             if (!response.ok) throw new Error("Error fetching image");
-
+    
             const data = await response.json();
             setImageUrl(data.url);
             setAuthor(data.author);
@@ -198,11 +207,18 @@ const Player = () => {
             setTags(tagsHandler(data.tags));
             setViews(data.views);
             setCurrentImageId(data._id);
-            setLikes(data.likes);
             setTextToCopy(`https://pixo-v1.netlify.app/${data._id}`);
-
-            console.log("Fetched image _id:", data._id);
-            return data._id
+    
+            const userId = getUserIdFromToken();
+            const likes = data.likes;
+            if (likes.some(like => like.toString() === userId)) {
+                setIsLiked(true);
+            } else {
+                setIsLiked(false);
+            }
+            setLikes(likes.length)
+            console.log("ID:", data._id);
+            return data._id;
         } catch (error) {
             console.error("Error:", error);
         } finally {
@@ -300,7 +316,8 @@ const Player = () => {
         if (audioRef.current && songlink) {
             audioRef.current.src = songlink;
             audioRef.current.play().catch((error) => {
-                console.error("Playback error:", error);
+                /* console.error("Playback error:", error); */
+                return
             });
         }
     }, [songlink]);
@@ -316,7 +333,7 @@ const Player = () => {
         if (token) {
             try {
                 const decodedToken = jwtDecode(token);
-                console.log(decodedToken);
+                /* console.log(decodedToken); */
                 return decodedToken?.id || "";
             } catch (error) {
                 console.error("no id found:", error);
@@ -328,11 +345,11 @@ const Player = () => {
     const handleLike = async () => {
         const userId = getUserIdFromToken();
         const imageId = currentImageId;
-        
+    
         if (!imageId || !userId) {
             return;
         }
-      
+    
         try {
             const imageResponse = await fetch(`https://pixo-backend-version-1-2.onrender.com/image/${imageId}`);
             if (!imageResponse.ok) {
@@ -341,12 +358,11 @@ const Player = () => {
                 alert("Unable to retrieve image data");
                 return;
             }
-
+    
             const imageData = await imageResponse.json();
-      
             const isLiked = imageData.likes.includes(userId);
-      
             const url = isLiked ? "https://pixo-backend-version-1-2.onrender.com/dislike-image" : "https://pixo-backend-version-1-2.onrender.com/like-image";
+            
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
@@ -354,24 +370,27 @@ const Player = () => {
                 },
                 body: JSON.stringify({ imageId, userId }),
             });
-      
+    
             if (!response.ok) {
                 const errorText = await response.text();
                 console.error("Error response:", errorText);
                 alert("Something went wrong. Please try again.");
                 return;
             }
-      
+    
             const data = await response.json();
-            setLikes(data.likes); 
+    
+            setLikes(data.likes);
             setIsLiked(!isLiked);
         } catch (error) {
             console.error("Error handling like/dislike:", error);
             alert("An error occurred. Please try again later.");
         }
     };
+    
 
     const volume = document.querySelector(".player-volume");
+    const likes_count = likes.length;
 
     const handleMouseEnter = () => {
         volume.style.opacity = "1";
@@ -412,9 +431,9 @@ const Player = () => {
             </div>
             <div onMouseEnter={handleMouseEnter} className={`player-volume ${IsOpacity ? "opacity0" : ""}`}>
                 {audio === 0 ? (
-                    <svg width="24" data-e2e="" height="24" viewBox="0 0 48 48" fill="#fff" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M25 10.8685C25 8.47242 22.3296 7.04325 20.3359 8.37236L10.3944 15H6C4.34315 15 3 16.3431 3 18V30C3 31.6568 4.34314 33 6 33H10.3944L20.3359 39.6276C22.3296 40.9567 25 39.5276 25 37.1315V10.8685ZM29.2929 18.1213L35.1716 24L29.2929 29.8787C28.9024 30.2692 28.9024 30.9024 29.2929 31.2929L30.7071 32.7071C31.0976 33.0976 31.7308 33.0976 32.1213 32.7071L38 26.8284L43.8787 32.7071C44.2692 33.0976 44.9024 33.0976 45.2929 32.7071L46.7071 31.2929C47.0976 30.9024 47.0976 30.2692 46.7071 29.8787L40.8284 24L46.7071 18.1213C47.0976 17.7308 47.0976 17.0976 46.7071 16.7071L45.2929 15.2929C44.9024 14.9024 44.2692 14.9024 43.8787 15.2929L38 21.1716L32.1213 15.2929C31.7308 14.9024 31.0976 14.9024 30.7071 15.2929L29.2929 16.7071C28.9024 17.0976 28.9024 17.7308 29.2929 18.1213Z"></path></svg>
+                    <svg width="24" data-e2e="" height="24" viewBox="0 0 48 48" fill="#fff" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M25 10.8685C25 8.47242 22.3296 7.04325 20.3359 8.37236L10.3944 15H6C4.34315 15 3 16.3431 3 18V30C3 31.6568 4.34314 33 6 33H10.3944L20.3359 39.6276C22.3296 40.9567 25 39.5276 25 37.1315V10.8685ZM29.2929 18.1213L35.1716 24L29.2929 29.8787C28.9024 30.2692 28.9024 30.9024 29.2929 31.2929L30.7071 32.7071C31.0976 33.0976 31.7308 33.0976 32.1213 32.7071L38 26.8284L43.8787 32.7071C44.2692 33.0976 44.9024 33.0976 45.2929 32.7071L46.7071 31.2929C47.0976 30.9024 47.0976 30.2692 46.7071 29.8787L40.8284 24L46.7071 18.1213C47.0976 17.7308 47.0976 17.0976 46.7071 16.7071L45.2929 15.2929C44.9024 14.9024 44.2692 14.9024 43.8787 15.2929L38 21.1716L32.1213 15.2929C31.7308 14.9024 31.0976 14.9024 30.7071 15.2929L29.2929 16.7071C28.9024 17.0976 28.9024 17.7308 29.2929 18.1213Z"></path></svg>
                 ) : (
-                    <svg width="24" data-e2e="" height="24" viewBox="0 0 48 48" fill="#fff" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M20.3359 8.37236C22.3296 7.04325 25 8.47242 25 10.8685V37.1315C25 39.5276 22.3296 40.9567 20.3359 39.6276L10.3944 33H6C4.34314 33 3 31.6568 3 30V18C3 16.3431 4.34315 15 6 15H10.3944L20.3359 8.37236ZM21 12.737L12.1094 18.6641C11.7809 18.8831 11.3948 19 11 19H7V29H11C11.3948 29 11.7809 29.1169 12.1094 29.3359L21 35.263V12.737ZM32.9998 24C32.9998 21.5583 32.0293 19.3445 30.4479 17.7211C30.0625 17.3255 29.9964 16.6989 30.3472 16.2724L31.6177 14.7277C31.9685 14.3011 32.6017 14.2371 33.0001 14.6195C35.4628 16.9832 36.9998 20.3128 36.9998 24C36.9998 27.6872 35.4628 31.0168 33.0001 33.3805C32.6017 33.7629 31.9685 33.6989 31.6177 33.2724L30.3472 31.7277C29.9964 31.3011 30.0625 30.6745 30.4479 30.2789C32.0293 28.6556 32.9998 26.4418 32.9998 24ZM37.0144 11.05C36.6563 11.4705 36.7094 12.0995 37.1069 12.4829C40.1263 15.3951 42.0002 19.4778 42.0002 23.9999C42.0002 28.522 40.1263 32.6047 37.1069 35.5169C36.7094 35.9003 36.6563 36.5293 37.0144 36.9498L38.3109 38.4727C38.6689 38.8932 39.302 38.9456 39.7041 38.5671C43.5774 34.9219 46.0002 29.7429 46.0002 23.9999C46.0002 18.2569 43.5774 13.078 39.7041 9.43271C39.302 9.05421 38.6689 9.10664 38.3109 9.52716L37.0144 11.05Z"></path></svg>
+                    <svg width="24" data-e2e="" height="24" viewBox="0 0 48 48" fill="#fff" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M20.3359 8.37236C22.3296 7.04325 25 8.47242 25 10.8685V37.1315C25 39.5276 22.3296 40.9567 20.3359 39.6276L10.3944 33H6C4.34314 33 3 31.6568 3 30V18C3 16.3431 4.34315 15 6 15H10.3944L20.3359 8.37236ZM21 12.737L12.1094 18.6641C11.7809 18.8831 11.3948 19 11 19H7V29H11C11.3948 29 11.7809 29.1169 12.1094 29.3359L21 35.263V12.737ZM32.9998 24C32.9998 21.5583 32.0293 19.3445 30.4479 17.7211C30.0625 17.3255 29.9964 16.6989 30.3472 16.2724L31.6177 14.7277C31.9685 14.3011 32.6017 14.2371 33.0001 14.6195C35.4628 16.9832 36.9998 20.3128 36.9998 24C36.9998 27.6872 35.4628 31.0168 33.0001 33.3805C32.6017 33.7629 31.9685 33.6989 31.6177 33.2724L30.3472 31.7277C29.9964 31.3011 30.0625 30.6745 30.4479 30.2789C32.0293 28.6556 32.9998 26.4418 32.9998 24ZM37.0144 11.05C36.6563 11.4705 36.7094 12.0995 37.1069 12.4829C40.1263 15.3951 42.0002 19.4778 42.0002 23.9999C42.0002 28.522 40.1263 32.6047 37.1069 35.5169C36.7094 35.9003 36.6563 36.5293 37.0144 36.9498L38.3109 38.4727C38.6689 38.8932 39.302 38.9456 39.7041 38.5671C43.5774 34.9219 46.0002 29.7429 46.0002 23.9999C46.0002 18.2569 43.5774 13.078 39.7041 9.43271C39.302 9.05421 38.6689 9.10664 38.3109 9.52716L37.0144 11.05Z"></path></svg>
                 )}
                 <input 
                     type="range" 
@@ -444,7 +463,7 @@ const Player = () => {
                     </span>
                 </div>
                 <li>
-                    <svg /* className={`like ${isLiked ? "liked" : ""}`} */ className='like' onClick={handleLike} width="24" height="24" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <svg className={`like ${isLiked ? "liked" : ""}`} /* className='like' */ onClick={handleLike} width="24" height="24" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                         <g clipPath="url(#HeartFill_clip0)">
                             <g filter="url(#HeartFill_filter0_d)">
                                 <path fillRule="evenodd" clipRule="evenodd" d="M7.5 2.25C10.5 2.25 12 4.25 12 4.25C12 4.25 13.5 2.25 16.5 2.25C20 2.25 22.5 4.99999 22.5 8.5C22.5 12.5 19.2311 16.0657 16.25 18.75C14.4095 20.4072 13 21.5 12 21.5C11 21.5 9.55051 20.3989 7.75 18.75C4.81949 16.0662 1.5 12.5 1.5 8.5C1.5 4.99999 4 2.25 7.5 2.25Z"></path>
@@ -493,7 +512,7 @@ const Player = () => {
                 <button className="close-button" onClick={e => { e.stopPropagation(); setIsVisible2(false); }}><svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M20.7457 3.32851C20.3552 2.93798 19.722 2.93798 19.3315 3.32851L12.0371 10.6229L4.74275 3.32851C4.35223 2.93798 3.71906 2.93798 3.32854 3.32851C2.93801 3.71903 2.93801 4.3522 3.32854 4.74272L10.6229 12.0371L3.32856 19.3314C2.93803 19.722 2.93803 20.3551 3.32856 20.7457C3.71908 21.1362 4.35225 21.1362 4.74277 20.7457L12.0371 13.4513L19.3315 20.7457C19.722 21.1362 20.3552 21.1362 20.7457 20.7457C21.1362 20.3551 21.1362 19.722 20.7457 19.3315L13.4513 12.0371L20.7457 4.74272C21.1362 4.3522 21.1362 3.71903 20.7457 3.32851Z" fill="#0F0F0F"></path> </g></svg></button>
                 <div className="flex-cnt">
                     <li>
-                        <h5>{likes}</h5>
+                        <h5>{likes_count}</h5>
                         <p>Likes</p>
                     </li>
                     <li>
