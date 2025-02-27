8614e3047fbd0551dba8154b16e195ef0d849264
S0dxu <159276909+S0dxu@users.noreply.github.com>
2025-02-27 18:41:33 +0100

    Update Player.jsx

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -536,12 +536,18 @@ const Player = () => {
                     </span>
                 </div>
                 <li>
-                    <svg className={`like ${isLiked ? "liked" : ""}`} /* className='like' */ onClick={handleLike} width="24" height="24" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                        <g clipPath="url(#HeartFill_clip0)">
-                            <g filter="url(#HeartFill_filter0_d)">
-                                <path fillRule="evenodd" clipRule="evenodd" d="M7.5 2.25C10.5 2.25 12 4.25 12 4.25C12 4.25 13.5 2.25 16.5 2.25C20 2.25 22.5 4.99999 22.5 8.5C22.5 12.5 19.2311 16.0657 16.25 18.75C14.4095 20.4072 13 21.5 12 21.5C11 21.5 9.55051 20.3989 7.75 18.75C4.81949 16.0662 1.5 12.5 1.5 8.5C1.5 4.99999 4 2.25 7.5 2.25Z"></path>
-                            </g>
-                        </g>
+                    <svg 
+                        className="like" 
+                        onClick={handleLike} 
+                        width="24" 
+                        height="24" 
+                        viewBox="0 0 24 24" 
+                        fill="#ff0000"  // Cambiato da currentColor a rosso
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path fillRule="evenodd" clipRule="evenodd" 
+                            d="M7.5 2.25C10.5 2.25 12 4.25 12 4.25C12 4.25 13.5 2.25 16.5 2.25C20 2.25 22.5 4.99999 22.5 8.5C22.5 12.5 19.2311 16.0657 16.25 18.75C14.4095 20.4072 13 21.5 12 21.5C11 21.5 9.55051 20.3989 7.75 18.75C4.81949 16.0662 1.5 12.5 1.5 8.5C1.5 4.99999 4 2.25 7.5 2.25Z">
+                        </path>
                     </svg>
                     <p>{likes}</p>
                 </li>
